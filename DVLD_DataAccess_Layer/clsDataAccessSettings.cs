aa483c9f1e578c9a7310ebb10b538f93f484b1d0
Abdalrhman_Darweesh <abdalrhman1367@yahoo.com>
2026-04-24 21:02:19 +0300

    initial Solution without code

---
+++ DVLD_DataAccess_Layer/clsDataAccessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess_Layer
+{
+    internal class clsDataAccessSettings
+    {
+        public string connectionString = "Server =.; Database = DVLDDB; User Id = sa; Password = sa123456;";
+    }
+}
---
+++ DVLD_DataAccess_Layer/clsPersonsDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess_Layer
+{
+    public class clsPersonsDataAccess
+    {
+
+        // Add 
+
+
+        // Select 
+
+
+        // Update
+        
+
+
+
+        // Delete
+    }
+}
